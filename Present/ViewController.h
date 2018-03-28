dad786a39f7fa6e3be04962004158780611ba9b5
mango <mango@mangodeMac-mini.local>
2018-03-28 09:31:51 +0800

    Initial Commit

---
+++ Present/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Present
+//
+//  Created by mango on 2018/3/28.
+//  Copyright © 2018年 mango. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Present/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Present
+//
+//  Created by mango on 2018/3/28.
+//  Copyright © 2018年 mango. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
